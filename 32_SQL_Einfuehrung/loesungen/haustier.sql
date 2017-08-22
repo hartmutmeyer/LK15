40e136e6be418a468a8e8fa0e7f95c6450d09618
hartmut meyer <h.meyer6@schule.bremen.de>
2017-08-22 17:27:12 +0200

    .

--- 32_SQL_Einfuehrung/loesungen/haustier.sql
+++ 32_SQL_Einfuehrung/loesungen/haustier.sql
@@ -132,3 +132,131 @@ ALTER TABLE tier
 
 # Übung 3, Aufgabe 1
 
+INSERT INTO besitzer VALUES
+(NULL, 'Firma', NULL,    'Zoo Lilliput', 'Obernstraße 54', 20012, 'Hamburg', '0721/343412'),
+(NULL, 'Frau', 'Sandra', 'Sandelmann', 'Kullerweg 12',     28205, 'Bremen', NULL),
+(NULL, 'Herr', 'Mirco',  'Sandelmann', 'Unterstraße 17',   28232, 'Bremen', '0421/123456');
+
+INSERT INTO besitzer (vorname, nachname) VALUES
+('Tobias', 'Winkelmann');
+
+INSERT INTO besitzer (anrede, vorname, nachname) VALUES
+('Frau', 'Sandra', 'Anderson');
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname IS NULL
+    AND nachname = 'Zoo Lilliput'
+)
+WHERE name = 'Bello';
+
+UPDATE tier
+SET tier_besitzer_id = ( 
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname IS NULL
+    AND nachname = 'Zoo Lilliput'
+)
+WHERE name = 'Lassie';
+
+UPDATE tier 
+SET tier_besitzer_id  = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Sandra'
+    AND nachname = 'Sandelmann'
+)
+WHERE name = 'Daisy' 
+AND tierart = 'Kanarienvogel';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Mirco'
+    AND nachname = 'Sandelmann'
+)
+WHERE name = 'Mausi';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Mirco'
+    AND nachname = 'Sandelmann'
+)
+WHERE name = 'Blacky';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Mirco'
+    AND nachname = 'Sandelmann'
+)
+WHERE name = 'Harald';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Tobias'
+    AND nachname = 'Winkelmann'
+)
+WHERE name = 'Daisy'
+AND tierart = 'Schildkröte';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Tobias'
+    AND nachname = 'Winkelmann'
+)
+WHERE name = 'Hasso';
+
+UPDATE tier
+SET tier_besitzer_id = (
+    SELECT besitzer_id FROM besitzer
+    WHERE vorname = 'Sandra'
+    AND nachname = 'Anderson'
+)
+WHERE name = 'Maja';
+
+# Übung 3, Aufgabe 2
+
+UPDATE besitzer
+SET straße = 'Wilhelminenweg 42', plz = 28315, ort = 'Bremen'
+WHERE nachname = 'Anderson';
+
+# Übung 3, Aufgabe 3
+
+# 3A) Zeige ohne Verwendung der WHERE-Klausel eine Tabelle mit allen Besitzern (Nachname 
+#     und Vorname) und Tieren (Name und Tierart) an. Was für eine Tabelle wird erstellt?
+
+
+# 3B) Zeige eine Liste mit allen Besitzern und den zu ihnen gehörenden Tieren an. Es sollen
+#     alle Spalten angezeigt werden.
+
+
+# 3C) Zeige eine Tabelle mit allen Besitzern (Nachname und Vorname) und ihren Tieren 
+#     (Name und Tierart) an. Sortiere die Liste in aufsteigender alphabetischer Reihenfolge
+#     zunächst nach dem Nachnamen und dann nach dem Vornamen.
+
+
+# 3D) Zeige eine Tabelle mit allen Besitzern (Vor- und Nachname) und ihren lebendigen Tieren
+#     an (Name und Tierart). Sortiere die Liste nach der Tierart.
+
+
+# 3E) Zeige alle Tiere von Mirco Sandelmann an (Name, Tierart und „lebendig“).
+
+
+# 3F) Wähle alle Besitzer von Hunden aus. Zeige Vor- und Nachname der Besitzer sowie
+#     Name, Geburtstag und Todestag des Hundes an.
+
+
+# 3G) Zeige eine Liste der Besitzer (Vor- und Nachname) und der Anzahl der Tiere an, die
+#     sie besitzen. Sortiere die Liste in umgekehrter alphabetischer Reihenfolge nach
+#     Nachnamen.
+
+
+# 3H) Zeige alle Besitzer mit Vor- und Nachnamen an, die zwei oder mehr Tiere besitzen.
+
+
+# 3I) Zähle die Anzahl der Besitzer, die einen Hund oder eine Katze besitzen.
+
+
